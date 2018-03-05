432641d1b65672e418eb196872858d1b705a8a75
Pranav Hippargi <pranavhippargi@gmail.com>
2018-03-05 10:28:41 -0800

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,6 +1,9 @@
+#Twitter API
 import tweepy
+#Pandas for DataFrame Objects
 import pandas as pd  
 
+# API KEYS 
 consumer_key = "yLjjxhLqUrp4181CRpF87XsrA"
 consumer_secret = "uO9jXZcCJ1eRPHnVLA1eWf9U5hqC3NLH5PPwQvuWDrC2kvGcnE"
 access_token = "2840569633-ve68zs1gAnS2qrwS3PVHYcPxgYCLV8oRdfoovDj"
@@ -22,34 +25,50 @@ class HelloWorld():
         api = tweepy.API(auth)
 
         # The search term you want to find
-        query = "Donald Trump"
+        query = "Microsoft"
         # Language code (follows ISO 639-1 standards)
         language = "en"
 
         # Calling the user_timeline function with our parameters
         results = api.search(q=query, lang=language)
 
-        # Create arrays to store 1) Tweet,  2) Location, 3) Screen Name\
+        # Create arrays to store 1) Tweet,  2) Location, 3) Screen Name
+        # All other things like Date + Time, Media, ect, will be added later once script is better. 
 
-        tweetsArray = []
-        locationArray = []
-        screenNameArray = []
+        self.tweetsArray = []
+        self.locationArray = []
+        self.screenNameArray = []
 
         # foreach through all tweets pulled
         for tweet in results:
         # printing the text stored inside the tweet object
-            #print(tweet.user.screen_name,"Tweeted:",tweet.text, "Location:", tweet.user.location)
-            tweetsArray.append(tweet.text)
-            locationArray.append(tweet.user.location)
-            screenNameArray.append(tweet.user.screen_name)
-       # print(tweetsArray)
-        #print(locationArray)
-        s1 = pd.Series(tweetsArray)
-        s2 = pd.Series(locationArray)
-        s3 = pd.Series(screenNameArray)
-        df = pd.join(s1, s2)
-        #print(df.head)   
-        df.to_csv('tweetsCSV2.csv', sep='\t', encoding='utf-8', index=False)    
+        #print(tweet.user.screen_name,"Tweeted:",tweet.text, "Location:", tweet.user.location)
+
+           self.tweetsArray.append(tweet.text)
+           self.locationArray.append(tweet.user.location)
+           self.screenNameArray.append(tweet.user.screen_name)
+      
+            
+    
+    def addToArray(self):    
+
+        # Creating Series to add to data frame
+        s1 = pd.Series(self.tweetsArray)
+        s2 = pd.Series(self.locationArray)
+        s3 = pd.Series(self.screenNameArray)
+
+        #Creating Data Frame
+        df = pd.concat([s1, s2, s3], axis=1)
+        print(df)
+
+        # Adding to CSV File for now. 
+        # Should have another method that adds to NoSQL Database 
+        df.to_csv('Microsoft2.csv', sep='\t', encoding='utf-8', index=False)    
+
+#_-----------------------------------------------------------------------------------------x-----------------------------------------------------------------------_#
+
+#Let's GO! 
 test = HelloWorld()
 test.getTweets()
+test.addToArray()
     
